9fe1cfa0637ca839bd1b565a3fcecb8938665657
Lucas Caribé <lucasmoc47@gmail.com>
2021-05-19 15:31:37 -0300

    Part 1

---
+++ exercises/4.1/exercise_1/exercise.js
@@ -0,0 +1,11 @@
+const name = "Lucas Caribé";
+
+const birthCity = "Montes Claros";
+
+let birthYear = 1992;
+
+console.log(name, birthCity, birthYear);
+
+birthYear = 1993;
+
+console.log(birthYear);

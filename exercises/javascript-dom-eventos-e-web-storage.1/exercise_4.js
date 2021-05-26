72d48d852ee6c69d1964dd66c3326721004401a1
Lucas Caribé <lucasmoc47@gmail.com>
2021-05-26 20:54:14 -0300

    Finishing exercises

--- exercises/javascript-dom-eventos-e-web-storage.1/exercise_4.js
+++
@@ -1,8 +0,0 @@
-// 4. Crie uma função que corrija o texto da tag <h1>.
-const title = document.getElementsByClassName('title');
-
-function fixH1(element) {
-    element.innerText = 'Exercício 5.1 - JavaScript';
-}
-
-fixH1(title[0]);

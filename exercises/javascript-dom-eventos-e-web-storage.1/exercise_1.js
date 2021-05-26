4c0f337163c3621679124435bceabfd6d2fb2ce2
Lucas Caribé <lucasmoc47@gmail.com>
2021-05-26 20:43:29 -0300

    Exercise 4

--- exercises/javascript-dom-eventos-e-web-storage.1/exercise_1.js
+++ exercises/javascript-dom-eventos-e-web-storage.1/exercise_1.js
@@ -1,7 +1,7 @@
 // 1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. 
 // (Não gaste tempo pensando no texto e sim realizando o exercício)
 
-let text = document.getElementsByTagName('p');
+const text = document.getElementsByTagName('p');
 
 text[0].innerText = "Como me vejo daqui a 2 anos";
 text[1].innerText = "Quero estar trabalhando como programador em uma grande empresa!";

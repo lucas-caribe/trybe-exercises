79bd091dacc74d4b99163421d6b6493778438b21
Lucas Caribé <lucasmoc47@gmail.com>
2021-05-28 01:08:49 -0300

    5.2 - Part 1, 2 and 3

--- exercises/5.2/script.js
+++ exercises/5.2/script.js
@@ -1,3 +1,4 @@
+// Part 1
 // 1. Acesse o elemento elementoOndeVoceEsta.
 const currentElement = document.querySelector('#elementoOndeVoceEsta');
 
@@ -13,20 +14,58 @@ firstChild.innerHTML = 'Aqui vai um texto bem legal';
 
 // 4. Acesse o primeiroFilho a partir de pai .
 const firstChildFromParent = parentElement.firstElementChild;
-console.log(firstChildFromParent);
+// console.log(firstChildFromParent);
 
 // 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
 const firstChildFromCurrentElement = currentElement.firstElementChild;
-console.log(firstChildFromCurrentElement);
+// console.log(firstChildFromCurrentElement);
 
 // 6. Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
 const attention = currentElement.nextSibling;
-console.log(attention);
+// console.log(attention);
 
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
 const thirdChildFromCurrentElement = currentElement.nextElementSibling;
-console.log(thirdChildFromCurrentElement); 
+// console.log(thirdChildFromCurrentElement);
 
 // 8. Agora acesse o terceiroFilho a partir de pai .
-const thirdChildFromParent = parentElement.firstElementChild.nextElementSibling.nextElementSibling;
-console.log(thirdChildFromParent);
+const thirdChildFromParent =
+	parentElement.firstElementChild.nextElementSibling.nextElementSibling;
+// console.log(thirdChildFromParent);
+
+// Part 2
+// 1. Crie um irmão para elementoOndeVoceEsta.
+const currentsSibling = document.createElement('div');
+currentsSibling.id = 'currents-sibling';
+parentElement.appendChild(currentsSibling);
+
+// 2. Crie um filho para elementoOndeVoceEsta.
+const currentsChild = document.createElement('div');
+currentsChild.id = 'currents-child';
+currentElement.appendChild(currentsChild);
+
+// 3. Crie um filho para primeiroFilhoDoFilho.
+const currentsChildsChild = document.createElement('div');
+currentsChildsChild.id = 'currents-child-child';
+currentsChild.appendChild(currentsChildsChild);
+
+// 4. A partir desse filho criado, acesse terceiroFilho.
+const thirdChildFromTheDeepWeb =
+	currentsChildsChild.parentElement.parentElement.nextElementSibling;
+// console.log(thirdChildFromTheDeepWeb);
+
+// Part 3
+function removeNodes(node) {
+    if(node === null) return;
+
+    removeNodes(node.firstChild);
+
+    removeNodes(node.nextSibling);
+
+    if(node.id !== 'pai' && node.id !== 'elementoOndeVoceEsta' && node.id !== 'primeiroFilhoDoFilho') {
+        console.log(`should remove ${node.id}`);
+        node.parentNode.removeChild(node);
+    }
+}
+
+removeNodes(parentElement);

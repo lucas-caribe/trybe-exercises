377a205dec1dd656e880bb0522acd9d56e173d84
Lucas Caribé <lucasmoc47@gmail.com>
2021-06-01 15:23:11 -0300

    5.4 - Basic styling + background and text functionalities

---
+++ exercises/javascript-dom-eventos-e-web-storage.4/constants.js
@@ -0,0 +1,25 @@
+const colorsList = [
+	'#ffffff',
+	'#f1f3f4',
+    '#fbfec3',
+	'#f8fd89',
+	'#eddd6e',
+	'#eed369',
+	'#edd1b0',
+	'#b5c588',
+	'#107895',
+	'#0d3c55',
+	'#083045',
+	'#000000',
+];
+
+const textTitle = 'Lorem Ipsum';
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pretium nisl eu feugiat volutpat. In hac habitasse platea dictumst. Pellentesque fermentum sagittis felis, id bibendum tortor ultrices nec. Maecenas id dui malesuada arcu tristique sodales. Nulla facilisi. Pellentesque feugiat sagittis porta. Proin leo tellus, tincidunt vel nibh at, congue pretium diam.
+
+Suspendisse elementum mollis libero, vitae hendrerit augue dignissim nec. Donec feugiat cursus velit, ut aliquam nisi consequat a. Etiam faucibus aliquam blandit. Donec a commodo libero. Phasellus id urna sit amet enim lobortis scelerisque et ac est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet sodales arcu, a finibus sapien porta a. Ut sed pellentesque lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla arcu metus, semper et elit at, pharetra tempus leo. Fusce ut ornare dolor. Integer consequat pellentesque mauris. Morbi in interdum sapien. Morbi suscipit lectus vel erat venenatis egestas. Vestibulum laoreet ex lacinia magna imperdiet fringilla.
+
+In sagittis ornare varius. Pellentesque quis gravida turpis. Nam eget velit at ligula ullamcorper tristique volutpat eget ligula. Sed vitae tincidunt nunc. Donec turpis nulla, euismod et nulla ac, ultricies laoreet neque. Cras rutrum accumsan metus, imperdiet dapibus quam laoreet eu. Sed pulvinar sagittis enim a consectetur. Vivamus consequat, nibh tempor lacinia cursus, nunc ligula pretium metus, id laoreet metus leo id diam. Nunc sed elementum nisi, accumsan consequat elit. Vivamus ac ligula ut purus hendrerit ullamcorper. Nullam urna nunc, feugiat vitae ornare et, dignissim quis dui. In ultrices suscipit turpis, quis porttitor orci ultricies eget. Duis gravida bibendum purus, ut imperdiet sem vehicula id. Suspendisse potenti. Integer et ligula sit amet ligula gravida pellentesque. Mauris lacus tellus, tincidunt sodales ipsum eget, pellentesque ultrices sem.
+
+Cras diam ipsum, cursus eu commodo mollis, viverra non eros. Nam cursus tincidunt ipsum. Vivamus faucibus ex odio, at tempor turpis malesuada in. Vivamus at lectus facilisis, tincidunt odio nec, tempus risus. Pellentesque lobortis felis vitae eros volutpat, et euismod ex laoreet. Duis at rutrum ligula. Integer scelerisque metus magna, ac vehicula ex porttitor a. Nam sit amet turpis risus. Suspendisse potenti. Nunc nec mi purus. Duis consectetur efficitur ipsum et tristique. Proin convallis elit eu nulla pellentesque, at pellentesque sem vehicula.
+
+Aenean blandit lectus a auctor porttitor. Curabitur enim ex, sodales iaculis lacus mollis, pharetra commodo nulla. Morbi consectetur, arcu eget placerat commodo, leo risus auctor arcu, vitae vehicula sem augue ut velit. Phasellus ornare nibh sed magna tincidunt vulputate. Sed efficitur scelerisque pretium. Nunc euismod diam sit amet neque imperdiet elementum. Aenean risus magna, accumsan eget quam eu, fringilla fringilla urna. Suspendisse eu facilisis neque, at laoreet est. Praesent ornare dolor ex, et feugiat ligula varius ornare. Mauris lacinia nulla non nulla finibus dapibus. Aliquam feugiat pulvinar justo, tempus condimentum eros vehicula eget. Aliquam a ultricies nisi. In leo urna, viverra sit amet massa vitae, gravida viverra nulla. Cras eu varius nisi.`;

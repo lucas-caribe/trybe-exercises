9dca461b04cb8f5ecd2c7857e5e42d83e52f35fa
Lucas Caribé <lucasmoc47@gmail.com>
2021-12-03 19:12:27 -0300

    Updating exercises

--- exercises/modulo-3/nodejs-fluxo-assincrono/exercicio6.js
+++ exercises/modulo-3/nodejs-fluxo-assincrono/exercicio6.js
@@ -7,15 +7,19 @@
 const readline = require('readline-sync');
 const fs = require('fs').promises;
 
-async function readFile() {
+async function getFile() {
   const filename = readline.question('Insira o nome do arquivo: ');
 
   try {
     const fileContent = await fs.readFile(filename, 'utf-8');
-    console.log(fileContent);
+    return fileContent;
   } catch (error) {
-    console.log('Arquivo inexistente');
+    return 'Arquivo inexistente';
   }
 }
 
-readFile();
+getFile()
+  .then((fileContent) => console.log(fileContent))
+  .catch((error) => console.log(error));
+
+module.exports = getFile;
